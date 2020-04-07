0d8ba8bebfbdce892c61c87d001e70bed8b124e1
Benson Tran <benson@bensons-mbp.lan>
2020-04-07 14:31:58 -0400

    Fixed tabbing and changed if statements to switch

--- src/components/UserInputParser.js
+++ src/components/UserInputParser.js
@@ -8,19 +8,23 @@ export const parseUserInput = (msg) => {
   let hiddenMenuKeys = Object.keys(hiddenMenus);
   let cleanedMsg = userInputCleaner(msg);
 
-  // Empty message
-  if (cleanedMsg === "") {
-    return "Cannot do anything with an empty command."
-  }
-
-  // Show command list
-  // Try some variations of "commands" just in case
-  if (cleanedMsg === "commands" || cleanedMsg === "command" || cleanedMsg === "commandlist") {
-    return getCommandList();
-  }
-
-  if ((cleanedMsg === "staredown")) {
-    return stareDownHandler(menuKeys, hiddenMenuKeys);
+  // Try special cases before matching input to menu list
+  switch (cleanedMsg) {
+    // Empty message
+    case "":
+      return "Cannot do anything with an empty command."
+    // Try some variations of "commands" for typos
+    case "commands":
+      return getCommandList();
+    case "command":
+      return getCommandList();
+    case "commandlist":
+      return getCommandList();
+    // Staredown path
+    case "staredown":
+      return stareDownHandler(menuKeys, hiddenMenuKeys);
+    default:
+      break;
   }
 
   // Match user to the enum of possible menu options
@@ -36,13 +40,13 @@ const userInputCleaner = (msg) => {
   return msg.replace(/ /g, '').toLowerCase();
 }
 
-  // Count the amount of staredowns, if the amount of stares reaches 5, return a secret message
-  const stareDownHandler = (menuKeys, hiddenMenuKeys) => {
-    if (stareDownCount  < 5) {
-      stareDownCount++;
-      return menus[menuKeys[0]].value
-    }
-    if (stareDownCount >= 5) {
-      return hiddenMenus[hiddenMenuKeys[0]].value
-    }
+// Count the amount of staredowns, if the amount of stares reaches 5, return a secret message
+const stareDownHandler = (menuKeys, hiddenMenuKeys) => {
+  if (stareDownCount  < 5) {
+    stareDownCount++;
+    return menus[menuKeys[0]].value
   }
+  if (stareDownCount >= 5) {
+    return hiddenMenus[hiddenMenuKeys[0]].value
+  }
+}
